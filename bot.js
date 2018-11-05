5cb9379739f9c2bba5371708ce64e358c080479d
hgdfgxsgsg <43437178+hgdfgxsgsg@users.noreply.github.com>
2018-11-05 14:42:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,5 +12,55 @@ var channel = "504315975588380672";//ايدي الروم
     },305);
 })
 
+client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+
+let args = message.content.split(" ").slice(1);
+
+if(!args) return message.channel.send('${prefix}say <words>');	
+
+
+
+if (command == "5") {
+
+if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
+
+
+
+message.channel.send(args.join("  "))
+
+    message.delete();
+
+  }
+
+});
+
+client.on("message", msg => {
+if(msg.content === "credit") {
+msg.channel.send("#credit")
+}
+});
+
+
+
+
+
+
+
+
+
+
+
 
 client.login("NTA1NjQ0NDg3ODU5NjM0MTg3.DrWloQ.smmi0AXp-_f3yTYTY9t9cKaMst0");
